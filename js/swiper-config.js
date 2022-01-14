c2d0eee09eea78c6d3624b91ee8ab0acf97c1d24
jacob kleiman <jacobakleiman@gmail.com>
2022-01-13 16:06:20 -0800

    styles pagination bullets

--- js/swiper-config.js
+++ js/swiper-config.js
@@ -1,5 +1,9 @@
 let swiper = new Swiper(".mySwiper", {
     pagination: {
       el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+          return `<span class="` + className + `"></span>`;
+      }
     },
   });
