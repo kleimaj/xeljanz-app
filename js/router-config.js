11c69accf5aa9413bd6afa9ef11021e99372c0d8
jacob kleiman <jacobakleiman@gmail.com>
2022-01-21 12:58:01 -0800

    adds ctCtrl controller

--- js/router-config.js
+++ js/router-config.js
@@ -15,10 +15,79 @@ app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
         })
         .state('MOA', {
             url: '/moa',
-            templateUrl: 'content/moa/moa.html'
+            templateUrl: 'content/moa/moa.html',
         })
         .state('CT', {
             url: '/clinical-trials',
-            templateUrl: 'content/ct/ct.html'
+            templateUrl: 'content/ct/ct-landing.html'
+        })
+        .state ('STUDY-DESIGN', {
+            url: '/clinical-trials/main',
+            templateUrl: 'content/ct/main.html',
+            controller: 'ctCtrl',
+            params: {
+                slideIndex: 0
+            }
+        })
+        .state ('ENDPOINTS', {
+            url: '/clinical-trials/main',
+            templateUrl: 'content/ct/main.html',
+            controller: 'ctCtrl',
+            params: {
+                slideIndex: 1
+            }
+        })
+        .state ('SECONDARY-SAFETY-ENDPOINTS', {
+            url: '/clinical-trials/main',
+            templateUrl: 'content/ct/main.html',
+            controller: 'ctCtrl',
+            params: {
+                slideIndex: 2
+            }
+        })
+        .state ('PATIENT-DEMOGRAPHICS', {
+            url: '/clinical-trials/main',
+            templateUrl: 'content/ct/main.html',
+            controller: 'ctCtrl',
+            params: {
+                slideIndex: 3
+            }
+        })
+        .state ('PHASE-PK-ANALYSIS', {
+            url: '/clinical-trials/main',
+            templateUrl: 'content/ct/main.html',
+            controller: 'ctCtrl',
+            params: {
+                slideIndex: 4
+            }
         })
 });
+
+app.controller("ctCtrl", function ($scope, $state, $stateParams) {
+    initSwiper($stateParams.slideIndex);
+});
+
+function initSwiper (slideIndex) {
+    var swiper = new Swiper('.mySwiper', {
+        initialSlide: slideIndex,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          renderBullet: function (index, className) {
+            return `<span class="` + className + `"></span>`;
+          },
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+      swiper.on('slideChange', function () {
+        if (swiper.isEnd)
+          document.querySelector('.review-btn').classList.remove('vis-hidden');
+        else {
+          document.querySelector('.review-btn').classList.add('vis-hidden');
+        }
+      });
+      
+}
