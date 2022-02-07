22ce383161fa2bc7b8daccf84807116c52ef1b90
jacob kleiman <jacobakleiman@gmail.com>
2022-02-06 16:34:54 -0800

    adds quiz to efficacy page

--- js/efficacy/modal.js
+++ js/efficacy/modal.js
@@ -7,41 +7,23 @@ var defaultSubmit = `<i class="fas fa-check"></i>
 SUBMIT`;
 var correctSubmit = `<i class="fas fa-times"></i>
 CLOSE`;
+var nextSubmit = `NEXT <i class="fas fa-chevron-right></i>`
 
-var secondQuestion = `A SIGNIFICANTLY LOWER PROPORTION OF
-PATIENTS TREATED WITH TOFACITINIB
-EXPERIENCED A DISEASE FLARE AT WEEK
-44 IN THE DB PHASE COMPARED TO THE
-PLACEBO GROUP`
-var thirdQuestion = `THE CLINICAL TRIAL STUDY DESIGN
-ENCOMPASSED 2 PHASES, THE
-OPEN-LABEL RUN-IN PHASE AND THE
-DOUBLE-BLIND PHASE; WHICH PATIENTS
-WERE RANDOMIZED TO THE
-DOUBLE-BLIND PHASE?`
 
-var fourthQuestion = `THE PRIMARY AND KEY SECONDARY
-ENDPOINTS IN THIS STUDY WERE:`
+var secondQuestion = `THE CLINICAL TRIAL STUDY DESIGN ENCOMPASSED 2 PHASES, THE OPEN-LABEL RUN-IN PHASE AND THE DOUBLE-BLIND PHASE; WHICH PATIENTS WERE RANDOMIZED TO THE DOUBLE-BLIND PHASE?`
 
-var answer3A = `PATIENTS ACHIEVING JIA ACR50
+var thirdQuestion = `THE PRIMARY AND KEY SECONDARY ENDPOINTS IN THIS STUDY WERE:`
+
+var answer2A = `PATIENTS ACHIEVING JIA ACR50 OR BETTER`
+var answer2B = `PATIENTS ACHIEVING JIA ACR 70`
+var answer2C = `PATIENTS ACHIEVING JIA ACR 30
 OR BETTER`
-var answer3B = `PATIENTS ACHIEVING JIA ACR 70`
-var answer3C = `PATIENTS ACHIEVING JIA ACR 30
-OR BETTER`
-var answer3D = `PATIENTS ACHIEVING NO
-IMPROVEMENT DURING THE OPEN-LABEL PHASE`
+var answer2D = `PATIENTS ACHIEVING NO IMPROVEMENT DURING THE OPEN-LABEL PHASE`
 
-var answer4A = `TIME TO DISEASE FLARE, JIA
-ACR30/50/70 RESPONSE
-RATES OVER TIME AND MEAN
-JADAS27-CRP OVER TIME`
-var answer4B = `OCCURRENCE OF DISEASE FLARE
-AND TIME TO DISEASE FLARE`
-var answer4C = `OCCURRENCE OF DISEASE FLARE, JIA
-ACR30/50/70 RESPONSE RATES, AND
-CHANGE FROM DB PHASE BASELINE
-IN CHAQ-DI`
-var answer4D = `NONE OF THE ABOVE`
+var answer3A = `TIME TO DISEASE FLARE, JIA ACR30/50/70 RESPONSE RATES OVER TIME AND MEAN JADAS27-CRP OVER TIME`
+var answer3B = `OCCURRENCE OF DISEASE FLARE AND TIME TO DISEASE FLARE`
+var answer3C = `OCCURRENCE OF DISEASE FLARE, JIA ACR30/50/70 RESPONSE RATES, AND CHANGE FROM DB PHASE BASELINE IN CHAQ-DI`
+var answer3D = `NONE OF THE ABOVE`
 
 var currentQuestion = 0;
 
@@ -69,20 +51,62 @@ var closeModal = () => {
   document.querySelector('.submit').addEventListener('click', checkAnswer);
 };
 
+var nextQuestion = () => {
+  // Hide result
+  document.querySelector('.result').classList.add('hidden');
+  // Reset submit button
+  document.querySelector('.submit').innerHTML = defaultSubmit;
+  document.querySelector('.submit').removeEventListener('click', nextQuestion);
+  document.querySelector('.submit').addEventListener('click', checkAnswer);
+  if (currentQuestion == 1) {
+    // Change modal to second question
+    document.querySelector('.modal h4').innerText = secondQuestion;
+    // Create two additional buttons, append to dom
+    for (let i = 0; i < 2; i++) {
+      var button = document.createElement('button')
+      button.className = 'modal-btn efficacy';
+      button.addEventListener('click', highlightButton)
+      document.querySelector('.btn-container').insertAdjacentElement("beforebegin",button);
+
+    }
+
+    var ansArr = [answer2A,answer2B,answer2C,answer2D];
+    document.querySelectorAll('.modal-btn').forEach((btn, idx) => {
+      btn.innerText = ansArr[idx];
+      btn.classList.remove('active');
+    })
+  }
+  else {
+    document.querySelector('.modal h4').innerText = thirdQuestion;
+    var ansArr = [answer3A,answer3B,answer3C,answer3D];
+    document.querySelectorAll('.modal-btn').forEach((btn, idx) => {
+      btn.innerText = ansArr[idx];
+      btn.classList.remove('active');
+    })
+  }
+}
+
 var checkAnswer = () => {
   if (
     document.querySelector('.modal-btn.active') &&
     document.querySelector('.modal-btn.active').innerText ==
-      'TRUE'
+      'TRUE' || document.querySelector('.modal-btn.active').innerText == answer2C || document.querySelector('.modal-btn.active').innerText== answer3C 
   ) {
+    currentQuestion++;
     document.querySelector('.result').innerHTML = correctResult;
     document.querySelector('.result').classList.remove('incorrect');
     document.querySelector('.result').classList.remove('hidden');
 
-    document.querySelector('.submit').innerHTML = correctSubmit;
     document.querySelector('.submit').removeEventListener('click', checkAnswer);
+    if (currentQuestion == 3) {
+    document.querySelector('.submit').innerHTML = correctSubmit;
     document.querySelector('.submit').addEventListener('click', closeModal);
-  } else {
+    }
+    else {
+      document.querySelector('.submit').addEventListener('click', nextQuestion);
+      document.querySelector('.submit').innerHTML = nextSubmit;
+    }
+  } else { //incorrect answer
     document.querySelector('.result').innerHTML = incorrectResult;
     document.querySelector('.result').classList.remove('hidden');
     document.querySelector('.result').classList.add('incorrect');
