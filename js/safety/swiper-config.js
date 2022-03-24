12f16c7d3c304e5b0b58cde5346977a3cbed7a25
jacob kleiman <jacobakleiman@gmail.com>
2022-03-24 14:41:33 -0700

    adds modal to safety carousel

--- js/safety/swiper-config.js
+++ js/safety/swiper-config.js
@@ -1,7 +1,13 @@
+var modalOpened = false;
 $( document ).ready(function() {
 var swiper = document.querySelector('.mySwiper').swiper;
 swiper.on('slideChange', function () {
     //   $('#ct.container').scrollTop(0);
+    if (swiper.activeIndex == 4 && !modalOpened) {
+      document.querySelector('.modal.info').classList.toggle('hidden');
+      modalOpened = true;
+      document.querySelector('.modal.info .submit').addEventListener('click', () => document.querySelector('.modal.info').classList.add('hidden'));
+    }
     document.querySelector('.mySwiper').scrollTop=0
     // console.log(document.querySelector('.swiper-slide-active').offsetHeight)
     if (swiper.isEnd)
